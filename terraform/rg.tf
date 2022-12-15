b68849ed01ead3f426ccfc1d73604d40a9abaf53
goldfinger3 <juergen.fechter@atos.net>
2022-12-15 21:43:37 +0100

    test

--- terraform/rg.tf
+++ terraform/rg.tf
@@ -1,7 +1,7 @@
 locals {
   #  Common tags to be assigned to all resources
   default_tags = {
-    creator    = "<%=email%>"
+    creator    = "<%=username%>"
     management_tool = "terraform"
     management_platform = "Morpheus"
   }
