20f49af53ab11a0ab4f4c4c191c237edaccd04a9
goldfinger3 <juergen.fechter@atos.net>
2022-12-15 17:09:15 +0100

    add Tag

--- terraform/rg.tf
+++ terraform/rg.tf
@@ -1,4 +1,18 @@
+locals {
+  #  Common tags to be assigned to all resources
+  default_tags = {
+    creator    = "<%=username.email%>"
+    management_tool = "terraform"
+    management_platform = "Morpheus"
+  }
+
 resource "azurerm_resource_group" "my" {
   name      = var.resource_group_name
   location  = var.location
+  tags = merge(
+    local.default_tags,
+    {
+      name = var.resource_group_name
+    }
+  )
 }
